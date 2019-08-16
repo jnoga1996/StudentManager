7e8214cd617a63d51d55969f734dd23d862a380b
Jacek <jaceknoga11@gmail.com>
2019-08-16 14:46:24 +0200

    Added CRUD operations to course controller

--- src/main/java/com/smanager/controllers/CourseController.java
+++ src/main/java/com/smanager/controllers/CourseController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,8 @@ public class CourseController {
     }
 
     @GetMapping("/Create")
-    public String showForm(Course course) {
+    public String showForm(Course course, Model model) {
+        model.addAttribute("isCreate", true);
         return "course_form";
     }
 
@@ -36,6 +38,42 @@ public class CourseController {
             return "course_form";
         }
         courseRepository.save(course);
+
+        return "redirect:/Course/Index";
+    }
+
+    @GetMapping("/Edit")
+    public String edit(Model model, Long id) {
+        Course course = courseRepository.getOne(id);
+        if (course == null) {
+            return "redirect:/Course/Index";
+        }
+        model.addAttribute("course", course);
+        model.addAttribute("isCreate", false);
+        model.addAttribute("id", id);
+        return "course_form";
+    }
+
+    @PostMapping("/Edit")
+    public String edit(@Valid Course course, Model model, BindingResult binding) {
+        if (binding.hasErrors()) {
+            return "redirect:/Course/Index";
+        }
+
+        Course courseFromDb = courseRepository.getOne(course.getId());
+        courseFromDb.setTitle(course.getTitle());
+        courseFromDb.setEcts(course.getEcts());
+        courseRepository.save(courseFromDb);
+
+        return "redirect:/Course/Index";
+    }
+
+    @GetMapping("/Delete")
+    public String delete(Long id) {
+        Course course = courseRepository.getOne(id);
+        if (course != null) {
+            courseRepository.delete(course);
+        }
 
         return "redirect:/Course/Index";
     }
