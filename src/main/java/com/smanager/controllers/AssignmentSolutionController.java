fb4b4382fae5f4c6c6a2434fecd57e5ef62b62c3
Jacek <jaceknoga11@gmail.com>
2019-08-29 21:35:15 +0200

    Added parametrized fragment for solution. Changed assignmentSolution controller.

--- src/main/java/com/smanager/controllers/AssignmentSolutionController.java
+++ src/main/java/com/smanager/controllers/AssignmentSolutionController.java
@@ -1,11 +1,18 @@
 package com.smanager.controllers;
 
+import com.smanager.dao.models.Assignment;
+import com.smanager.dao.models.AssignmentSolution;
+import com.smanager.dao.models.Solution;
 import com.smanager.dao.repositories.AssignmentSolutionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/AssignmentSolution")
@@ -16,7 +23,23 @@ public class AssignmentSolutionController {
 
     @GetMapping("Index")
     public String index(Model model) {
-        model.addAttribute("assignmentSolutions", assignmentSolutionRepository.findAll());
+        List<AssignmentSolution> assignmentSolutionList = assignmentSolutionRepository.findAll();
+        HashMap<Assignment, List<Solution>> assignmentSolutionsMap = new HashMap<>();
+        List<AssignmentSolution> distinctAssignmentSolutionList = new LinkedList<>();
+
+        for (AssignmentSolution asol : assignmentSolutionList) {
+            if (!assignmentSolutionsMap.containsKey(asol.getAssignment())) {
+                List<Solution> solutions = new LinkedList<>();
+                solutions.add(asol.getSolution());
+                assignmentSolutionsMap.put(asol.getAssignment(), solutions);
+                distinctAssignmentSolutionList.add(asol);
+            } else {
+                assignmentSolutionsMap.get(asol.getAssignment()).add(asol.getSolution());
+            }
+        }
+        
+        model.addAttribute("assignmentSolutions", distinctAssignmentSolutionList);
+        model.addAttribute("solutionsForAssignment", assignmentSolutionsMap);
         return "assignmentSolution_index";
     }
 }
--- src/main/java/com/smanager/dao/repositories/SolutionRepository.java
+++ src/main/java/com/smanager/dao/repositories/SolutionRepository.java
@@ -2,6 +2,14 @@ package com.smanager.dao.repositories;
 
 import com.smanager.dao.models.Solution;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SolutionRepository extends JpaRepository<Solution, Long> {
+    @Query(value = "select * from solutions s " +
+            "join assignment_solutions ass on s.solution_id = ass.solution_id " +
+            "where assignment_id = id", nativeQuery = true)
+    List<Solution> getAllByAssignmentId(@Param("id")Long id);
 }
